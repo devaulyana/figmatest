7d21200320d9b6854ec98ece064b211f0d633104
ulianadelmar <devaulyana@gmail.com>
2023-11-13 18:18:12 +0300

    dev: I add materialize

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var sidnave = document.querySelectorAll('.sidenav');
+  var instances__sidnave = M.Sidenav.init(sidnave);
+  var modal = document.querySelectorAll('.modal');
+  var instances__modal = M.Modal.init(modal);
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var gallery = document.querySelectorAll('.materialboxed');
+  var instances__gallery = M.Materialbox.init(gallery);
+});
